423c539b7ce1547910f12720eed3025ae08eb3f9
Sean Edwards <seantrevoredwards@gmail.com>
2020-12-23 07:15:30 +0000

    added second rspec test, added code to reverse_sequence.rb to pass tests

--- lib/reverse_sequence.rb
+++ lib/reverse_sequence.rb
@@ -1,3 +1,3 @@
 def reverse_sequence(array)
-  [5,4,3,2,1]
+  array.reverse
 end
--- spec/reverse_sequence_spec.rb
+++ spec/reverse_sequence_spec.rb
@@ -1,7 +1,13 @@
 require 'reverse_sequence'
 
 describe 'reverse_sequence' do
+
   it 'returns the given array in reverse sequence' do
     expect(reverse_sequence([1,2,3,4,5])).to eq [5,4,3,2,1]
   end
+
+  it 'returns the given array in reverse sequence' do
+    expect(reverse_sequence([10,9,8,7,6,5,4,3,2,1])).to eq [1,2,3,4,5,6,7,8,9,10]
+  end
+  
 end
