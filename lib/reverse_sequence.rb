cb030e3b62830a3b9caf958e75bf017453f73e88
Sean Edwards <seantrevoredwards@gmail.com>
2020-12-23 07:21:09 +0000

    added third rspec test for string outliers and refactored code in method to pass tests

--- lib/reverse_sequence.rb
+++ lib/reverse_sequence.rb
@@ -1,3 +1,4 @@
 def reverse_sequence(array)
+  return "That is not an array!" if !array.is_a?(Array)
   array.reverse
 end
--- spec/reverse_sequence_spec.rb
+++ spec/reverse_sequence_spec.rb
@@ -9,5 +9,9 @@ describe 'reverse_sequence' do
   it 'returns the given array in reverse sequence' do
     expect(reverse_sequence([10,9,8,7,6,5,4,3,2,1])).to eq [1,2,3,4,5,6,7,8,9,10]
   end
-  
+
+  it 'returns "That is not an array!" when passed a String as an arguement' do
+    expect(reverse_sequence("")).to eq "That is not an array!"
+  end
+
 end
